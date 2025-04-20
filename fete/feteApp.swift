88faea2dece89af88073215cb0b9a794fbc1006f
Raidel <15664418+raidel-a@users.noreply.github.com>
2025-04-20 17:10:09 -0400

    refactor: remove unused Item model and clean up model container

--- fete/feteApp.swift
+++ fete/feteApp.swift
@@ -12,19 +12,6 @@ import SwiftData
 struct feteApp: App {
     @StateObject private var authViewModel = AuthenticationViewModel()
     
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             if authViewModel.isAuthenticated {
